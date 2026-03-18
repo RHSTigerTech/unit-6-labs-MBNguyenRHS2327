3fef11e3902a37dfc819d2a9f8edd78fc2846b5b
Julie <malinanguyen23@outlool.com>
2026-03-18 13:06:38 -0700

    sales updates

--- src/Sales/Sales.java
+++ src/Sales/Sales.java
@@ -1,8 +1,9 @@
 package Sales;
+
 /**
- * Name:
- * Date:
- * Period:
+ * Name: Juliette Nguyen
+ * Date: 17/3/26
+ * Period: 3
  *
  * Sales.Sales.java
  *
@@ -10,88 +11,98 @@ package Sales;
  * sales entered by salesperson id and total sales for all salespeople.
  */
 
-import java.util.Arrays;
-import java.util.Scanner;
 import Util.*;
 
-public class Sales
-{
-    public static void main(String[] args)
-    {
+public class Sales {
+  public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+    // TODO 4) Prompt the user for the number of sales people that they need to
+    // enter
+    // TODO 4) and modify the program to create an appropriately sized array.
 
-        //TODO 4) Prompt the user for the number of sales people that they need to enter
-        //TODO 4) and modify the program to create an appropriately sized array.
+    final int SALESPEOPLE = MyTools.readInt("Enter number of sales persons for the day: ");
+    int[] sales = new int[SALESPEOPLE];
+    int sum;
 
-        
-        final int SALESPEOPLE = MyTools.readInt("Enter number of sales persons for the day: ");
-        int[] sales = new int[SALESPEOPLE];
-        int sum;
+    for (int i = 0; i < sales.length; i++) {
+      sales[i] = MyTools.readInt("Enter sales for salesperson " + (i + 1) + ": ");
+    }
+    // print the array for verification
+    // System.out.println(Arrays.toString(sales));
 
-        for (int i=0; i < sales.length; i++)
-        {
-            sales[i] = MyTools.readInt("Enter sales for salesperson " + (i + 1) + ": ");
-        }
-        // print the array for verification
-        // System.out.println(Arrays.toString(sales));
-
-        System.out.println("\nSalesperson   Sales");
-        System.out.println("--------------------");
-        sum = 0;
-        for (int i=0; i < sales.length; i++)
-        {
-            System.out.println("\t " + (i+1) + "\t\t\t" + sales[i]);
-            sum += sales[i];
-        }
-
-        System.out.println("\nTotal sales: " + sum);
-
-
-        //TODO 1b) Use your average method to print the average sales
-        System.out.println(average(sales));
-
-        //TODO 2b) Use your indexOfMax method to print the index of the best sales person
-        //TODO 2b) and the sales amount for that sales person.
-        System.out.println(indexOfMax(sales));
-        System.out.println(sales[indexOfMax(sales)]);
-
-        //TODO 3b) Do the same for the worst sales person
-
-
-
+    System.out.println("\nSalesperson   Sales");
+    System.out.println("--------------------");
+    sum = 0;
+    for (int i = 0; i < sales.length; i++) {
+      System.out.println((i + 1) + "\t\t" + sales[i]);
+      sum += sales[i];
     }
 
-    public static double average(int[] arr) throws IllegalStateException
-    {
-        //TODO 1a) compute and return the average of arr
-        //Be sure to use the length instance variable and you may assume that the array is full.
-        //If the array does not have at least 1 element in it, throw an IllegalStateException
-        if (arr.length<1){
-          throw new IllegalStateException("Array must have at least one statement to calculate the average");
-        }
-        double sum = 0.0;
-        for (int num:arr){
-          sum+=num;
-        }
-        return sum/arr.length;
+    System.out.println("\nTotal sales: " + sum);
+
+    // TODO 1b) Use your average method to print the average sales
+    System.out.println("Today's average sales: " + average(sales) + " sales.");
+
+    // TODO 2b) Use your indexOfMax method to print the index of the best sales
+    // person
+    // TODO 2b) and the sales amount for that sales person.
+    System.out.print("Sales person " + (indexOfMax(sales) + 1) + " had the best sales at ");
+    System.out.println(sales[indexOfMax(sales)] + " sales.");
+
+    // TODO 3b) Do the same for the worst sales person
+    System.out.print("Sales person " + (indexOfMin(sales) + 1) + " had the worst sales at ");
+    System.out.println(sales[indexOfMin(sales)] + " sales.");
+
+  }
+
+  public static double average(int[] arr) throws IllegalStateException {
+    // TODO 1a) compute and return the average of arr
+    // Be sure to use the length instance variable and you may assume that the array
+    // is full.
+    // If the array does not have at least 1 element in it, throw an
+    // IllegalStateException
+    if (arr.length < 1) {
+      throw new IllegalStateException("Array must have at least one statement to calculate the average");
     }
 
-    public static int indexOfMax(int[] arr)
-    {
-        //TODO 2a) find and return the index of the max value in arr
-        int max = Integer.MIN_VALUE;
+    double sum = 0.0;
 
-        for (int i = 0; i <arr.length;i++){
-          if (arr[i]>max){
-            max = i;
-          }
-        }
-
-        return max;
+    for (int num : arr) {
+      sum += num;
     }
 
-    //TODO 3a) Do the same for an indexOfMin method
+    return sum / arr.length;
+  }
 
+  public static int indexOfMax(int[] arr) {
+    // TODO 2a) find and return the index of the max value in arr
+    int max = Integer.MIN_VALUE;
+    int maxIndex = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        maxIndex = i;
+      }
+    }
+
+    return maxIndex;
+  }
+
+  // TODO 3a) Do the same for an indexOfMin method
+  public static int indexOfMin(int[] arr) {
+    // TODO 2a) find and return the index of the max value in arr
+    int min = Integer.MAX_VALUE;
+    int minIndex = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+      }
+    }
+
+    return minIndex;
+  }
 
 }
